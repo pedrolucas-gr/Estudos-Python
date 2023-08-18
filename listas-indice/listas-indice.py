bfeb12e38f93f994619394bb2d70f9090ec23db1
Pedro Lucas <pedrolucaspbi@gmail.com>
2023-08-18 09:43:17 -0300

    Exibe os índices da lista

---
+++ listas-indice/listas-indice.py
@@ -0,0 +1,11 @@
+"""
+Exercício
+Exiba os índices da lista
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
